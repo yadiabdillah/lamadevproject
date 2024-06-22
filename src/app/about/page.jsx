bb7c93bfe3f475cc33cbe5e8e670cfc817d9d69b
yadi abdillah <lasudmail@gmail.com>
2024-06-22 22:29:40 +0700

    about page and contact page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,9 +2,33 @@ import Image from "next/image";
 import styles from "./about.module.css"
 const aboutPage = () => {
     return (<div>
-      <div className={styles.imgContainer}>
-      <Image src="/about.png" alt="" fill/>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h2 className={styles.subtitle}>About Agency</h2>
+          <h1 className={styles.title}>We Create Digital Ideas That Are Bigger, Bolder, Braver and Better</h1>
+          <p className={styles.desc}>
+          Lorem ipsum dolor sit amet,
+           consectetur adipiscing elit. Vestibulum in posuere orci. Integer massa elit, placerat ut eros et, eleifend iaculis metus. Mauris quis aliquet diam. Aliquam ante velit, sagittis non aliquam eget, egestas ut massa. In tempor hendrerit laoreet. Suspendisse ex enim, aliquam quis pulvinar eget, condimentum ut nisi. Aliquam vehicula lacus vel ligula eleifend volutpat. Nullam at rutrum nunc. Duis suscipit congue lectus id tristique. Aliquam id mauris iaculis, consectetur urna a, condimentum purus. Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+          </p>
+          <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year Of Experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year Of Experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year Of Experience</p>
+          </div>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="/about.png" alt="Picture Of The author" fill  className={styles.img}/>
+        </div>
+     </div>
     </div>
     );
   };
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,19 @@
+import styles from "./contact.module.css"
+
 const contactPage = () => {
-    return <div>Contact Page</div>;
+    return 
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+      <Image src="contact.png" alt="" fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name And Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number" />
+        </form>
+      </div>
+      </div>;
   };
   
   export default contactPage;
