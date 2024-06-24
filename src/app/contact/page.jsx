5d708ff47fd33913462f53c26e57ebcc6cfaa9c2
yadi abdillah <lasudmail@gmail.com>
2024-06-24 13:21:53 +0700

    add new

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,19 +1,22 @@
 import styles from "./contact.module.css"
+import Image from "next/image";
 
 const contactPage = () => {
-    return 
+    return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-      <Image src="contact.png" alt="" fill />
+      <Image src="/contact.png" alt="" fill />
       </div>
       <div className={styles.formContainer}>
         <form action="" className={styles.form}>
           <input type="text" placeholder="Name And Surname" />
           <input type="text" placeholder="Email Address" />
           <input type="text" placeholder="Phone Number" />
+        <textarea name="" id="" cols="30" rows="10" placeholder="Messages"></textarea>
+        <button className={styles.btnSend}>Send</button>
         </form>
       </div>
-      </div>;
-  };
+      </div>
+  )};
   
   export default contactPage;
